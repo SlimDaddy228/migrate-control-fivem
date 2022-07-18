7cfe9dda784fc28315e0bc38a9e5912f92eead34
SlimDaddy228 <60612282+SlimDaddy228@users.noreply.github.com>
2022-07-19 01:30:00 +0200

    :hammer: add exports for build label (framework used)

--- client/exports.lua
+++ client/exports.lua
@@ -1,3 +1,7 @@
 exports('requestNetworkControl', function(netId, duration)
     Base:requestNetworkControl(netId, duration)
 end)
+
+exports('buildLabel', function(entity)
+    Debugger:buildLabel(entity)
+end)
